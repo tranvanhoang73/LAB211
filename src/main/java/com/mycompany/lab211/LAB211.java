ab258f031d5a6de89e8c1ad616da53de7effe5fe
Tran Van Hoang <hoangtvh47@gmail.com>
2024-12-01 15:53:19 +0700

    fouth commit

--- src/main/java/com/mycompany/lab211/LAB211.java
+++ src/main/java/com/mycompany/lab211/LAB211.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.lab211;
 
@@ -8,9 +6,10 @@ package com.mycompany.lab211;
  *
  * @author Admin
  */
-public class LAB211 { 
+public class LAB211 {  
 
     public static void main(String[] args) {
         System.out.println("Hello all the World!");
+        System.out.println("Hello  you!");
     }
 }
