d99b5c998dbbf879d4e86ce086718b33d1fde264
tranvanhoang73 <hoangtvh73@gmail.com>
2024-12-01 15:44:35 +0700

    Update LAB211.java

--- src/main/java/com/mycompany/lab211/LAB211.java
+++ src/main/java/com/mycompany/lab211/LAB211.java
@@ -8,9 +8,9 @@ package com.mycompany.lab211;
  *
  * @author Admin
  */
-public class LAB211 {
+public class LAB211 { 
 
     public static void main(String[] args) {
-        System.out.println("Hello all World!");
+        System.out.println("Hello all the World!");
     }
 }
