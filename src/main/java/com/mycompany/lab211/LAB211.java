0ff6443d7d8d0709e9e714220070f45d6f4b157a
Tran Van Hoang <hoangtvh47@gmail.com>
2024-12-01 15:32:27 +0700

    first commit

---
+++ src/main/java/com/mycompany/lab211/LAB211.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab211;
+
+/**
+ *
+ * @author Admin
+ */
+public class LAB211 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
